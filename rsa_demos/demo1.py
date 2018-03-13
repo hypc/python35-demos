aa5bbac97f9365f762019dbe14e5353bfafa8b7f
hypc <h_yp00@163.com>
2018-03-13 13:17:41 +0800

    update rsa_demos

--- rsa_demos/demo1.py
+++ rsa_demos/demo1.py
@@ -11,35 +11,44 @@ class TestRSAUtils(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
         key = RSA.generate(2048)
-        self.public_key = key.publickey().exportKey()
-        self.private_key = key.exportKey()
-        print(self.public_key)
-        print(self.private_key)
+        self.private_key, self.public_key = RSAUtils.generate_keys()
 
-    @skip('')
+    def test_generate(self):
+        private_key, public_key = RSAUtils.generate_keys()
+        self.assertIsInstance(private_key, str)
+        self.assertIsInstance(public_key, str)
+
     def test_sign(self):
         message = b'1234'
         sign = RSAUtils.sign(message, self.private_key)
-        print(sign)
+        self.assertIsInstance(sign, str)
 
-    @skip('')
+    def test_sign_2(self):
+        message = '1234'
+        sign = RSAUtils.sign(message, self.private_key)
+        self.assertIsInstance(sign, str)
+
     def test_verify(self):
         message = b'1234'
         sign = RSAUtils.sign(message, self.private_key)
         verify = RSAUtils.verify(message, sign, self.public_key)
-        print(verify)
+        self.assertIsInstance(verify, bool)
+        self.assertTrue(verify)
+        verify = RSAUtils.verify(message.decode(), sign.encode(), self.public_key)
+        self.assertIsInstance(verify, bool)
+        self.assertTrue(verify)
 
-    @skip('')
     def test_encrypt(self):
         message = b'1234'
         ciphertext = RSAUtils.encrypt(message, self.public_key)
-        print(ciphertext)
+        self.assertIsInstance(ciphertext, str)
+        ciphertext = RSAUtils.encrypt(message.decode(), self.public_key)
+        self.assertIsInstance(ciphertext, str)
 
     def test_decrypt(self):
         message = b'1234'
         ciphertext = RSAUtils.encrypt(message, self.public_key)
         message2 = RSAUtils.decrypt(ciphertext, self.private_key)
-        print(message2)
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertIsInstance(message2, str)
+        message2 = RSAUtils.decrypt(ciphertext.encode(), self.private_key)
+        self.assertIsInstance(message2, str)
--- rsa_demos/rsa.py
+++ rsa_demos/rsa.py
@@ -11,13 +11,39 @@ from Crypto.Signature import PKCS1_v1_5 as Signature
 class RSAUtils(object):
 
     @classmethod
+    def generate_keys(cls):
+        """
+        :return: private_key and public_key
+        :rtype: (str, str)
+        """
+        key = RSA.generate(2048)
+        private_key = key.exportKey().decode()
+        public_key = key.publickey().exportKey().decode()
+        return private_key, public_key
+
+    @classmethod
     def sign(cls, message, private_key):
+        """
+        :type message: str | bytes
+        :type private_key: str | bytes
+        :rtype: str
+        """
+        if isinstance(message, str):
+            message = message.encode()
         h = SHA.new(message)
         signer = Signature.new(RSA.importKey(private_key))
         return base64.b64encode(signer.sign(h)).decode()
 
     @classmethod
     def verify(cls, message, sign, public_key):
+        """
+        :type message: str | bytes
+        :type sign: str | bytes
+        :type public_key: str | bytes
+        :rtype: bool
+        """
+        if isinstance(message, str):
+            message = message.encode()
         sign = base64.b64decode(sign)
         h = SHA.new(message)
         verifier = Signature.new(RSA.importKey(public_key))
@@ -25,11 +51,23 @@ class RSAUtils(object):
 
     @classmethod
     def encrypt(cls, message, public_key):
+        """
+        :type message: str | bytes
+        :type public_key: str | bytes
+        :rtype: str
+        """
+        if isinstance(message, str):
+            message = message.encode()
         cipher = Cipher.new(RSA.importKey(public_key))
         return base64.b64encode(cipher.encrypt(message)).decode()
 
     @classmethod
     def decrypt(cls, ciphertext, private_key):
+        """
+        :type ciphertext: str | bytes
+        :type private_key: str | bytes
+        :rtype: str
+        """
         ciphertext = base64.b64decode(ciphertext)
         sentinel = Random.new().read(SHA.digest_size)
         cipher = Cipher.new(RSA.importKey(private_key))
